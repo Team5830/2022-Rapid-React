8518e64c617c1a4f2cddccf022a6c85513fad2b4
Johnwesley R <jwr12135@gmail.com>
2022-02-03 21:12:07 -0500

    Feat: add flywheel subsytem

--- src/main/java/frc/robot/commands/SmartDashboardCmd.java
+++ src/main/java/frc/robot/commands/SmartDashboardCmd.java
@@ -8,13 +8,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Flywheel;
 
 public class SmartDashboardCmd extends CommandBase {
   private final DriveTrain m_subsystemDriveTrain;
+  private final Flywheel m_subsystemFlywheel;
 
   /** Creates a new SmartDashboard. */
-  public SmartDashboardCmd(DriveTrain m_subsystemDriveTrain) {
+  public SmartDashboardCmd(DriveTrain m_subsystemDriveTrain, Flywheel m_subsystemFlywheel) {
     this.m_subsystemDriveTrain = m_subsystemDriveTrain;
+    this.m_subsystemFlywheel = m_subsystemFlywheel;
+
+    SmartDashboard.putNumber("P Gain", this.m_subsystemFlywheel.pidVals.kP);
+    SmartDashboard.putNumber("I Gain", this.m_subsystemFlywheel.pidVals.kI);
+    SmartDashboard.putNumber("D Gain", this.m_subsystemFlywheel.pidVals.kD);
+    SmartDashboard.putNumber("I Zone", this.m_subsystemFlywheel.pidVals.kIz);
+    SmartDashboard.putNumber("Feed Forward", this.m_subsystemFlywheel.pidVals.kFF);
+    SmartDashboard.putNumber("Max Output", this.m_subsystemFlywheel.pidVals.kMaxOutput);
+    SmartDashboard.putNumber("Min Output", this.m_subsystemFlywheel.pidVals.kMinOutput);
+    SmartDashboard.putNumber("Shooter Velocity", this.m_subsystemFlywheel.pidVals.motorspeed);
   }
 
   // Called when the command is initially scheduled.
@@ -28,6 +40,16 @@ public class SmartDashboardCmd extends CommandBase {
     SmartDashboard.putNumber("Encoder Distance", m_subsystemDriveTrain.getAverageDistance());
     SmartDashboard.putNumber("Right Encoder Distance", m_subsystemDriveTrain.getRightDistance());
     SmartDashboard.putNumber("Left Encoder Distance", m_subsystemDriveTrain.getLeftDistance());
+
+    this.m_subsystemFlywheel.pidVals.kP = SmartDashboard.getNumber("P Gain", 0);
+    this.m_subsystemFlywheel.pidVals.kI = SmartDashboard.getNumber("I Gain", 0);
+    this.m_subsystemFlywheel.pidVals.kD = SmartDashboard.getNumber("D Gain", 0);
+    this.m_subsystemFlywheel.pidVals.kIz = SmartDashboard.getNumber("I Zone", 0);
+    this.m_subsystemFlywheel.pidVals.kFF = SmartDashboard.getNumber("Feed Forward", 0);
+    this.m_subsystemFlywheel.pidVals.kMaxOutput = SmartDashboard.getNumber("Max Output", 0);
+    this.m_subsystemFlywheel.pidVals.kMinOutput = SmartDashboard.getNumber("Min Output", 0);
+    this.m_subsystemFlywheel.pidVals.motorspeed = SmartDashboard.getNumber("Shooter Velocity", 0);
+    SmartDashboard.putBoolean("Shooter On", this.m_subsystemFlywheel.getShooterState());
   }
 
   // Called once the command ends or is interrupted.
