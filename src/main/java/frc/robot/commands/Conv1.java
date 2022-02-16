cbd47da1af553a3ca8ad5ba0892633969bca0ab4
EstrellianLuchador <sdrin0824@gmail.com>
2022-02-15 21:24:31 -0500

    Thasss Isss Awesssome

--- src/main/java/frc/robot/commands/Conv1.java
+++ src/main/java/frc/robot/commands/Conv1.java
@@ -1,19 +1,18 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.*;
-import frc.robot.Constants.*;
-import frc.robot.commands.*;
 import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Conv1 {
-  
+public class Conv1 extends CommandBase {
+    Conveyor spriva;
     public Conv1(Conveyor SprivA) {
+        spriva =SprivA;
+        addRequirements(spriva);
     }
   
     @Override
-    public boolean isFinished() {
-      // End when the controller is at the reference.
-      return getController().atSetpoint();
+    public void execute() {
+      spriva.DigiConvey1();
     }
   }
   
