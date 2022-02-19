1c38f1fd6275f6fd1ec95f1eb7e8713857571361
Aaron <asteele11@yahoo.com>
2022-02-19 13:56:18 -0500

    Conveyor motors are actually brushed

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -22,8 +22,8 @@ public class Conveyor extends SubsystemBase {
 
   public Conveyor(){
     try{
-      m_conv1motor = new CANSparkMax(CANBusID.conveyor1, MotorType.kBrushless);
-      m_conv2motor = new CANSparkMax(CANBusID.conveyor2, MotorType.kBrushless);
+      m_conv1motor = new CANSparkMax(CANBusID.conveyor1, MotorType.kBrushed);
+      m_conv2motor = new CANSparkMax(CANBusID.conveyor2, MotorType.kBrushed);
     } catch (RuntimeException ex) {
       DriverStation.reportError("Error instantiating conveyor motors " + ex.getMessage(), true);
     }
