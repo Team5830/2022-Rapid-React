9606b03b5854c9262bf6e42a9bc8e9ac96e27f16
Johnwesley R <jwr12135@gmail.com>
2022-02-19 16:30:02 -0500

    Fix shoot command

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -6,16 +6,17 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class Shoot extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final Flywheel m_flywheel;
   private final Conveyor m_conveyor;
   private boolean turnedON = false;
+
   public Shoot(Flywheel subsystemFLY, Conveyor subsystemCO) {
     m_flywheel = subsystemFLY;
     m_conveyor = subsystemCO;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_flywheel, m_conveyor);
-    
+
   }
 
   // Called when the command is initially scheduled.
@@ -27,13 +28,13 @@ public class Shoot extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      if (m_flywheel.readyToShoot() & m_conveyor.ballsensor2.get()){
-        m_conveyor.conveyor2ON();
-        turnedON = true;
-      }
-      if (turnedON & m_conveyor.ballsensor2.get()){
-        m_conveyor.conveyor2OFF();
-      }
+    if (m_flywheel.readyToShoot() && m_conveyor.ballsensor2.get()) {
+      m_conveyor.conveyor2ON();
+      turnedON = true;
+    }
+    if (turnedON && m_conveyor.ballsensor2.get()) {
+      m_conveyor.conveyor2OFF();
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -46,10 +47,6 @@ public class Shoot extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (turnedON & m_conveyor.ballsensor2.get()){
-        return true;
-    } else{
-        return false;
-    }
-    }
+    return turnedON && m_conveyor.ballsensor2.get();
+  }
 }
