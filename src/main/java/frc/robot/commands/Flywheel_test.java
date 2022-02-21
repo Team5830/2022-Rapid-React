60bb04e35a55c0fad7fd46c2dfced449436e4013
asteele11 <45502926+asteele11@users.noreply.github.com>
2022-02-21 13:20:17 -0500

    Shooter cmd (#23)
    * Add additional commands
    
    * Add more commands for Dashboard
    
    * Adding Shooter command
    
    * adding try catches
    
    * "actually add buttons"
    
    * Add test command for tuning shooter PID
    
    * jjjjjhh'

--- src/main/java/frc/robot/commands/Flywheel_test.java
+++ src/main/java/frc/robot/commands/Flywheel_test.java
@@ -1,53 +1,40 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Flywheel;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class Shoot extends CommandBase {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+public class Flywheel_test extends CommandBase {
   private final Flywheel m_flywheel;
-  private final Conveyor m_conveyor;
-  private boolean turnedON = false;
-
-  public Shoot(Flywheel subsystemFLY, Conveyor subsystemCO) {
+  public Flywheel_test(Flywheel subsystemFLY) {
     m_flywheel = subsystemFLY;
-    m_conveyor = subsystemCO;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(m_flywheel, m_conveyor);
+    addRequirements(m_flywheel);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_flywheel.shooteron();
+    //Get values from dashboard if not available use values from constants
+    m_flywheel.updatePIDValues();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-      if (m_flywheel.readyToShoot() & m_conveyor.ballsensor2.get()){
-        m_conveyor.conveyor2ON();
-        turnedON = true;
-      }
-      if (turnedON & m_conveyor.ballsensor2.get()){
-        m_conveyor.conveyor2OFF();
-      }
-  }
+    m_flywheel.shooteron();
+    }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_conveyor.conveyor2OFF();
     m_flywheel.shooteroff();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (turnedON & m_conveyor.ballsensor2.get()){
+    if (m_flywheel.readyToShoot()){
         return true;
     } else{
         return false;
--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -4,28 +4,35 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.*;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class Conveyor extends SubsystemBase {
     public boolean conveyor1ON = false;
     public boolean conveyor1Reversed = false;
     public boolean conveyor2ON = false;
     public boolean conveyor2Reversed = false;
+    /*
     CANSparkMax m_conv1motor;
     CANSparkMax m_conv2motor;
+    */
+    WPI_VictorSPX m_conv1motor;
+    WPI_VictorSPX m_conv2motor;
     public DigitalInput ballsensor1;
     public DigitalInput ballsensor2;
 
   public Conveyor(){
     try{
-      m_conv1motor = new CANSparkMax(CANBusID.conveyor1, MotorType.kBrushed);
-      m_conv2motor = new CANSparkMax(CANBusID.conveyor2, MotorType.kBrushed);
+      //m_conv1motor = new CANSparkMax(CANBusID.conveyor1, MotorType.kBrushed);
+      //m_conv2motor = new CANSparkMax(CANBusID.conveyor2, MotorType.kBrushed);
+      m_conv1motor = new WPI_VictorSPX(CANBusID.conveyor1);
+      m_conv2motor = new WPI_VictorSPX(CANBusID.conveyor2);
     } catch (RuntimeException ex) {
       DriverStation.reportError("Error instantiating conveyor motors " + ex.getMessage(), true);
     }
@@ -87,6 +94,12 @@ public class Conveyor extends SubsystemBase {
 
 
     public void DigiConvey1(){
+      //Might want to add a debouncer to smooth transition
+      // Debouncer m_debouncer = new Debouncer(0.1, Debouncer.DebounceType.kBoth);
+      // So if currently false the signal must go true for at least .1 seconds before being read as a True signal.
+      //if (m_debouncer.calculate(input.get())) {
+        // Do something now that the DI is True.
+      //}
       if (ballsensor1.get()) {
         conveyor1ON();
       } else {
