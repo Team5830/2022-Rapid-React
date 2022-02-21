f598eaf38953b8adf7d796de6ffc27b17fcea71b
EstrellianLuchador <sdrin0824@gmail.com>
2022-02-21 14:18:55 -0500

    nahhh

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,10 +2,10 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
+/*import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.ExternalFollower;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;   
+*/import com.revrobotics.CANSparkMaxLowLevel.MotorType;   
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
