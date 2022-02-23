0d29429d3b90d87d776b5bb62470a472f0a0252e
EstrellianLuchador <sdrin0824@gmail.com>
2022-02-22 20:51:53 -0500

    gahhahhahahahahahaahahahahah!!!!

--- src/main/java/frc/robot/subsystems/FirstIntake.java
+++ src/main/java/frc/robot/subsystems/FirstIntake.java
@@ -3,16 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-//import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.math.controller.PIDController;
-/*
+//import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.RelativeEncoder;
+//import edu.wpi.first.wpilibj.Encoder;
+//import edu.wpi.first.math.controller.PIDController;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-*/
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,54 +20,49 @@ import frc.robot.Constants.*;
 public class FirstIntake extends SubsystemBase {
   public boolean firstIntakeON = false;
   public boolean firstIntakeReversed = false;
-  /*
+  
   CANSparkMax m_intakemotor;
   CANSparkMax m_exotor;
   SparkMaxPIDController m_pidController;
-  */
-  WPI_VictorSPX m_intakemotor;
-  WPI_VictorSPX m_exotor;
-  PIDController m_pidController;
-  //RelativeEncoder m_encoder;
-  Encoder m_encoder;
+  RelativeEncoder m_encoder;
+  //Encoder m_encoder;
   boolean isExtended = false;
   public FirstIntake(){
     try{
-      m_intakemotor = new WPI_VictorSPX(CANBusID.dintakemotor); //new CANSparkMax(CANBusID.dintakemotor, MotorType.kBrushless);
-      m_exotor  = new WPI_VictorSPX(CANBusID.dexotor);         //new CANSparkMax(CANBusID.dexotor, MotorType.kBrushless);
-      Encoder m_encoder = new Encoder(5, 6); //Temporary
-      m_pidController = new PIDController(MovePID.P, MovePID.I, MovePID.D);
-      //m_exotor.restoreFactoryDefaults();
-      //m_intakemotor.restoreFactoryDefaults();
-      //m_pidController= m_exotor.getPIDController();
-      //m_encoder = m_exotor.getEncoder();
+      m_intakemotor = new CANSparkMax(CANBusID.dintakemotor, MotorType.kBrushless);
+      m_exotor = new CANSparkMax(CANBusID.dexotor, MotorType.kBrushless);
+      //RelativeEncoder m_encoder = new Encoder(5, 6); //Temporary
+      //m_pidController = new PIDController(MovePID.P, MovePID.I, MovePID.D);
+      m_exotor.restoreFactoryDefaults();
+      m_intakemotor.restoreFactoryDefaults();
+      m_pidController= m_exotor.getPIDController();
+      m_encoder = m_exotor.getEncoder();
       
     } catch (RuntimeException ex) {
       DriverStation.reportError("error loading failed" + ex.getMessage(), true);
     }
-    // set PID coefficients
-    /*
+     //set PID coefficients
+    
     m_pidController.setP(firstIntake.P);
     m_pidController.setI(firstIntake.I);
     m_pidController.setD(firstIntake.D);
     m_pidController.setIZone(firstIntake.zI);
     m_pidController.setFF(firstIntake.F);
     m_pidController.setOutputRange(firstIntake.kMinOutput, firstIntake.kMaxOutput);
-    */
     }
     
   public void extendIntake(){
    if (!isExtended){
-    //m_pidController.setReference(firstIntake.ExtendDistance, ControlType.kPosition);
-    m_pidController.setSetpoint(firstIntake.ExtendDistance);
+    m_pidController.setReference(firstIntake.ExtendDistance, ControlType.kPosition);
+    //m_pidController.setReference(firstIntake.ExtendDistance,Control);
     isExtended = true;
    }
   }
   
   public void retractIntake(){
     if (isExtended){
-      //m_pidController.setReference(0.0, ControlType.kPosition);
-      m_pidController.setSetpoint(0.0);
+      m_pidController.setReference(0.0, ControlType.kPosition);
+      //m_pidController.setSetpoint(0.0);
       isExtended = false;
     }
   }
