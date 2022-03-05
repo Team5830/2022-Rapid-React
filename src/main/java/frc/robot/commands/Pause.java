16e5a5e13ee1b428b66958325e5af2424fe06c02
EstrellianLuchador <sdrin0824@gmail.com>
2022-03-05 07:50:22 -0500

    thisss isss paussse

---
+++ src/main/java/frc/robot/commands/Pause.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Pause extends InstantCommand{
+    public Pause(){
+        super();
+    }
+    @Override
+    public void initialize(){
+        new Thread(){
+            public void run(){
+                Timer.delay(0.25);
+            }
+        }.start();
+    }
+}
